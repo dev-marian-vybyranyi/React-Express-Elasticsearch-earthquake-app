3238447b25e3d348396fc6b71dbb5f9da74d25aa
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-03-27 08:55:13 +0200

    Implement Elasticsearch API key authentication and client setup, replacing the old config file and `config` dependency.

---
+++ server/elasticsearch/client.js
@@ -0,0 +1,17 @@
+const { Client } = require("@elastic/elasticsearch");
+
+const client = new Client({
+  cloud: {
+    id: process.env.ELASTIC_CLOUD_ID,
+  },
+  auth: {
+    apiKey: process.env.ELASTIC_API_KEY,
+  },
+});
+
+client
+  .ping()
+  .then((response) => console.log("You are connected to Elasticsearch!"))
+  .catch((error) => console.error("Elasticsearch is not connected.", error));
+
+module.exports = client;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const client = require("./elasticsearch/client");
 
 const app = express();
 
